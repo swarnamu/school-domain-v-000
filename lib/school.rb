968e323876e4950785ccf4370135939ea119d812
Swarna Muralidharan <swarna.muralic@gmail.com>
2018-01-25 20:41:17 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,7 +1,13 @@
 # code here!
 class School
   roaster = {}
+  
   def initialize(school)
     @school = school
+  end
+
+  def add_student(name, grade)
+    @name = name
+    @grade = grade
   end
 end
